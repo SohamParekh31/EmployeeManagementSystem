c80a67be536ec61f8a2f037fe7fb5104b86407e6
SohamParekh31 <sohamparekh@promactinfo.com>
2020-09-01 11:03:43 +0530

    Removed unwanted comments

--- EmployeeManagementSystem/Models/MockDepartmentList.cs
+++ EmployeeManagementSystem/Models/MockDepartmentList.cs
@@ -27,26 +27,9 @@ namespace EmployeeManagementSystem.Models
             }
             con.Close();
         }
-        //static MockDepartmentList()
-        //{
-        //     dept = new List<Department>()
-        //    {
-        //        //new Department() { DepartmentId = "1",Name = "HR"},
-        //        //new Department() { DepartmentId = "2",Name = "Engineer"},
-        //    };
-        //}
         
         public List<Department> getDepartments()
         {
-            //SqlCommand cmd = new SqlCommand("select * from Departments", con);
-            //SqlDataReader reader = cmd.ExecuteReader();
-            //while (reader.Read())
-            //{
-            //    departments = new Department();
-            //    departments.DepartmentId = Convert.ToInt32(reader[0]);
-            //    departments.Name = reader[1].ToString();
-            //    dept.Add(departments);
-            //}
             return dept;
         }
 
@@ -64,7 +47,6 @@ namespace EmployeeManagementSystem.Models
             cmd.Parameters.AddWithValue("@DepartmentId", department.DepartmentId);
             cmd.Parameters.AddWithValue("@Name", department.Name);
             cmd.ExecuteNonQuery();
-            //dept.Add(department);
             con.Close();
         }
         public void UpdateDepartment(int id,Department department)
@@ -74,14 +56,6 @@ namespace EmployeeManagementSystem.Models
             SqlCommand cmd = new SqlCommand(query, con);
             cmd.ExecuteNonQuery();
             con.Close();
-            //Department updateDepartment = dept.Find(x => x.DepartmentId == department.DepartmentId);
-            //updateDepartment.DepartmentId = department.DepartmentId;
-            //updateDepartment.Name = department.Name;
-            //foreach (var item in _employee.getEmployees().ToList())
-            //{
-            //    if (item.department.DepartmentId == department.DepartmentId)
-            //        item.department.Name = department.Name;
-            //}
         }
         public void DeleteDepartment(int id)
         {
@@ -95,13 +69,6 @@ namespace EmployeeManagementSystem.Models
             cmd = new SqlCommand(query, con);
             cmd.ExecuteNonQuery();
             con.Close();
-            //Department department = dept.Find(x => x.DepartmentId == id);
-            //dept.Remove(department);
-            //foreach (var item in _employee.getEmployees().ToList())
-            //{
-            //    if (item.department.DepartmentId == id)
-            //        _employee.getEmployees().Remove(item);
-            //}
         }
 
         public Department getDepartmentById(int id)
